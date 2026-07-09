7f011fdbc0e72f29cc72c096fba3323098fd5fcd
Aryan Tiwari <aryantiwaribhu67@gmail.com>
2026-07-10 01:13:21 +0530

    Added partial search and similarity scores

--- recommender.py
+++ recommender.py
@@ -1,6 +1,10 @@
 import joblib
 movie_matrix=joblib.load("models/movie_matrix.joblib")
 similarity=joblib.load("models/similarity.joblib")
+matrix_titles=movie_matrix.index
+matrix_dict={i.lower():i for i in matrix_titles}
+
+#recommend function
 def recommend(movie_name):
     index=movie_matrix.index.get_loc(movie_name)
     movie_list=similarity[index]
@@ -9,16 +13,33 @@ def recommend(movie_name):
     movie_list=movie_list[1:6]
     recommendations=[]
     for movie in movie_list:
-       recommendations.append(movie_matrix.index[movie[0]])
+       recommendations.append((movie_matrix.index[movie[0]],movie[1]))
     return recommendations
-movie_name=input("Enter Movie Name : ")
 
-try: 
- result=recommend(movie_name)
- print("\n Recommended Movies:\n")
+#search function
+def search(inpt):
+    matches=[value for key,value in matrix_dict.items() if inpt in key]
+    return matches   
+
+usr_inpt=input("Enter Movie Name : ").lower()
+matches=search(usr_inpt)
+result=[]
+
+if len(matches)==1:
+   result=recommend(matches[0])
+elif len(matches)==0:
+  print("movie not found")
+else:
+  print("Choose one from below :")
+  for i in range(len(matches)):
+    print(i+1,matches[i])
+  selected_indx=int(input("\nenter the choice : ")) 
+  result=recommend(matches[selected_indx-1])
+
+print("\n Recommended Movies:\n")
  
- for i in result:
-    print(i)
-except KeyError:
-   print("Movie not found")
+for name,score in result:
+    print("Name : ",name,"    score : ", score)
+    
+
   
