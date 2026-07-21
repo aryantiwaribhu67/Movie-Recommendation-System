4722f8eb97d81fc7cfc85a2118920679677ed7e1
Aryan Tiwari <aryantiwaribhu67@gmail.com>
2026-07-22 01:25:14 +0530

    removed unwanted files

--- test_links.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-links=pd.read_csv("data/links.csv")
-print(links.head())
-print()
-print(links.columns)
-print
-print(links.shape)
-
--- test_merge.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-movies=pd.read_csv("data/movies.csv")
-links=pd.read_csv("data/links.csv")
-merged=pd.merge(movies,links,on="movieId")
-print(merged.head())
-print(merged.columns)
-print(merged.shape)
